8c7f29b6b007e2c6541ec957714894e7a52f2c02
hladikov_denys <gladikov.denis@gmail.com>
2018-11-20 22:58:30 +0200

    Improving plotting engine. Adding random moon placing on canvas. Adding sight switching button.

--- renderer.js
+++ renderer.js
@@ -2,46 +2,64 @@ const sun = new Image();
 const moon = new Image();
 const earth = new Image();
 
+const CANVAS_SIZE = {width: 400, height: 400};
+
+let isLookFromTop = true;
+
 const objects = {};
+
+var canvas;
 
 function init() {
     sun.src = 'https://mdn.mozillademos.org/files/1456/Canvas_sun.png';
     moon.src = 'https://mdn.mozillademos.org/files/1443/Canvas_moon.png';
     earth.src = 'https://mdn.mozillademos.org/files/1429/Canvas_earth.png';
 
-    objects.earth = {rotateDiv: [60, 60000], translate: [105, 0], drawImage: [earth, -12, -12]};
-    objects.moon = {rotateDiv: [6, 6000], translate: [0, 28.5], drawImage: [moon, -3.5, -3.5]};
+    objects.moon = [moon, 0, 0];
 
-
+    canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+    ctx.fillStyle = 'rgba(0,0,0,0.2)';
+    ctx.strokeStyle = 'rgba(0,153,255,0.2)';
+    ctx.save();
     setInterval(drawAllObjects, 10);
 }
 
-function drawAllObjects() {
-    var ctx = document.getElementById('canvas').getContext('2d');
-
-    ctx.globalCompositeOperation = 'destination-over';
-    ctx.clearRect(0, 0, 300, 300); // clear canvas
-
-    ctx.fillStyle = 'rgba(0,0,0,0.4)';
-    ctx.strokeStyle = 'rgba(0,153,255,0.4)';
-    ctx.save();
-    ctx.translate(150, 150);
-
-    var time = new Date();
-    Object.keys(objects).forEach(k => drawObject(ctx, objects[k], time));
-
-    ctx.restore();
-    ctx.restore();
-
-    ctx.beginPath();
-    ctx.arc(150, 150, 105, 0, Math.PI * 2, false); // Earth orbit
-    ctx.stroke();
-
-    ctx.drawImage(sun, 0, 0, 300, 300);
+function replaceMoon() {
+    randomMoon();
 }
 
-function drawObject(ctx, body, time) {
-    ctx.rotate(((2 * Math.PI) / body.rotateDiv[0]) * time.getSeconds() + ((2 * Math.PI) / body.rotateDiv[1]) * time.getMilliseconds());
-    ctx.translate(body.translate[0], body.translate[1]);
-    ctx.drawImage(body.drawImage[0], body.drawImage[1], body.drawImage[2]);
+function drawAllObjects() {
+    const ctx = canvas.getContext('2d');
+
+    ctx.globalCompositeOperation = 'destination-over';
+    ctx.clearRect(0, 0, CANVAS_SIZE.width, CANVAS_SIZE.height); // clear canvas
+    drawEarthOrbit();
+    drawMoon();
+    ctx.drawImage(sun, 0, 0, CANVAS_SIZE.width, CANVAS_SIZE.height);
+}
+
+function drawMoon() {
+    var ctx = canvas.getContext('2d');
+    ctx.drawImage(moon, objects.moon[1], objects.moon[2]);
+}
+
+function randomMoon() {
+    objects.moon[1] = Math.random() * CANVAS_SIZE.width;
+    objects.moon[2] = Math.random() * CANVAS_SIZE.height;
+}
+
+function changeSight() {
+    isLookFromTop = !isLookFromTop;
+}
+
+function drawEarthOrbit() {
+    const ctx = canvas.getContext('2d');
+    ctx.beginPath();
+    if (isLookFromTop) {
+        ctx.arc(CANVAS_SIZE.width / 2, CANVAS_SIZE.height / 2, CANVAS_SIZE.height / 4, 0, Math.PI * 2, false); // Earth orbit
+    } else {
+        ctx.rect(CANVAS_SIZE.height / 4, CANVAS_SIZE.width / 2, CANVAS_SIZE.width / 2, 1);
+    }
+    ctx.stroke();
 }
